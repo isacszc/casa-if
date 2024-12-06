8d7f6d2863b23bbd18980e231b0e5344e2d4a7e2
Isac <isacszc@hotmail.com>
2024-12-06 14:08:54 -0300

    alterei a atv 4

--- tsi4/Atividade_04/Atividade_04/urls.py
+++ tsi4/Atividade_04/Atividade_04/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from core.views import nome
+from core.views import nome, idade
 
 urlpatterns = [
-    path('nome/', nome, name='nome')
+    path('nome/', nome, name='nome'),
+    path('idade/', idade, name='idade'),
 ]
--- tsi4/Atividade_04/core/views.py
+++ tsi4/Atividade_04/core/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def nome(request):
-    return render(request, 'nome.html')
+    nome_usuario = 'Isac'
+    return render(request, 'nome.html', {'name':nome_usuario})
+
+def idade(request):
+    return render(request, 'idade.html')
